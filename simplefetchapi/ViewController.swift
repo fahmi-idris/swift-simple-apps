60d1278f0dca7729382a59052aa5284e7310c155
Frontend Developer <fidriz5@gmail.com>
2018-07-05 17:57:21 +0700

    Initial Commit

---
+++ simplefetchapi/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  simplefetchapi
+//
+//  Created by Fahmi Idris on 05/07/18.
+//  Copyright © 2018 Fahmi Idris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
